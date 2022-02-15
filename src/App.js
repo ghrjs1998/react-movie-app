7143db7a5eb651ec4052975db9b403829d8bd494
Oper <75073299+ghrjs1998@users.noreply.github.com>
2022-02-15 14:29:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import Movie from "./Movie";
+import Movie from "./Movie"; //movies.map으로부터 리턴할거 import한다
 import "./App.css"
 
 class App extends React.Component {
@@ -13,17 +13,18 @@ class App extends React.Component {
       data: {
         data: { movies }
       }
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    } = await axios.get( // await는 async 함수 안에서만 동작
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating" // sort_by=rating을 사용해 movie를 정랼
     );
-    this.setState({ movies, isLoading: false });
+    this.setState({ movies, isLoading: false }); // movies를 state안에 넣기
   };
-  componentDidMount() {
+  componentDidMount() { // component가 mount되면 getMovie를 호출
     this.getMovies();
   }
   render() {
     const { isLoading, movies } = this.state;
     return (
+      // javascript에선 class를 쓰지만 react에서는 class 대신 className을 씀
       <section className="container">
         {isLoading ? ( 
           <div className="loader">
@@ -40,7 +41,7 @@ class App extends React.Component {
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
                   genres={movie.genres}
-                  />
+                  /> //movies.map을 이용해 render한다
               ))}
             </div>
            )}
